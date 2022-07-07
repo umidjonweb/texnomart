eb16a10afc50eddd8edb47009f8d839529801cf6
umidjonweb <umidjon.web.03@gmail.com>
2022-07-07 10:58:29 +0500

    first commit

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 # texnomart
 # texno
 # texno
+# texno
---
+++ src/views/HomeView.vue
@@ -0,0 +1,141 @@
+  <template>
+  <div class="form">
+    <el-card class="el-card">
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="120px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="Password" prop="pass">
+          <el-input
+            type="tel"
+            v-mask="'+#####-###-##-##'"
+            v-model="ruleForm.tel"
+            placeholder="+_____ - ___ - __ -__"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Birth date" prop="pass">
+          <el-input
+            v-mask="'##/##/####'"
+            placeholder="__ / __ / ____"
+            type="text"
+            v-model="ruleForm.text"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Password" prop="pass">
+          <el-input
+            type="password"
+            v-model="ruleForm.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Confirm" prop="checkPass">
+          <el-input
+            type="password"
+            v-model="ruleForm.checkPass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="Age" prop="age">
+          <el-input v-model.number="ruleForm.age"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >Submit</el-button
+          >
+          <el-button @click="resetForm('ruleForm')">Reset</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("Please input the age"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("Please input digits"));
+        } else {
+          if (value < 18) {
+            callback(new Error("Age must be greater than 18"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please input the password"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please input the password again"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("Two inputs don't match!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        pass: "",
+        checkPass: "",
+        age: "",
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        text: [{ validator: validatePass, trigger: "blur" }],
+        tel: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+<style lang="scss" >
+.form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+}
+.el-card__body {
+  padding: 30px 50px 30px 0px !important;
+  padding-left: 0 !important;
+}
+.el-card {
+  width: 700px;
+}
+</style>
